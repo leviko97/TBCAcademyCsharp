3fee75adc98a3c1904fa73ff42630b0fb6a6931b
Levan Tetemadze <ltetemadze@tbcbank.com.ge>
2023-06-05 18:54:24 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Pleas write temperature");
+double temp = Convert.ToDouble(Console.ReadLine());
+
+if (temp < 0 )
+{
+    Console.WriteLine("Freezing");
+}
+else if (temp < 30)
+{
+    Console.WriteLine("Good weather");
+}
+else
+{
+    Console.WriteLine("Toooo hot");
+}
+
